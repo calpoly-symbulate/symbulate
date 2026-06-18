2f3d23f051c3230159fbd1a4a1827f19967d87e7
lkhant17 <linkhant.roland@gmail.com>
2026-06-18 12:28:38 -0700

    documentation for random processes

--- symbulate/random_processes.py
+++ symbulate/random_processes.py
@@ -28,6 +28,21 @@ class RandomProcess(RV):
 
     def __init__(self, prob_space, index_set=Naturals(),
                  func=lambda outcome, t: outcome[t]):
+        """Initialize a RandomProcess.
+
+        Parameters
+        ----------
+        prob_space : ProbabilitySpace
+            The underlying probability space for the random process.
+        index_set : IndexSet, optional
+            The index set (collection of times) for the random process.
+            Defaults to the natural numbers 0, 1, 2, 3, ...
+        func : callable, optional
+            A function ``(outcome, t)`` that returns the value of the process
+            at time ``t`` for a given ``outcome``. Defaults to
+            ``lambda outcome, t: outcome[t]``, which treats each outcome as
+            a function of time.
+        """
         self.index_set = index_set
         # This dict stores random variables at specific times.
         self.rvs = {}
@@ -44,6 +59,21 @@ class RandomProcess(RV):
         super().__init__(prob_space, _func)
 
     def __setitem__(self, t, value):
+        """Set the random variable or constant value of the process at time ``t``.
+
+        Parameters
+        ----------
+        t : int or float
+            The time index at which to set the value. Must be in the
+            process's index set.
+        value : RV or scalar
+            The random variable or scalar constant to assign at time ``t``.
+
+        Raises
+        ------
+        KeyError
+            If ``t`` is not in the index set of the random process.
+        """
         if t not in self.index_set:
             raise KeyError(
                 "Time %s is not in the index set for this "
@@ -57,10 +87,34 @@ class RandomProcess(RV):
             self.rvs[t] = RV(self.prob_space, lambda outcome: value)
 
     def __getitem__(self, t):
+        """Get the random variable representing the process at time ``t``.
+
+        Parameters
+        ----------
+        t : int or float
+            The time index to retrieve.
+
+        Returns
+        -------
+        RV
+            The random variable ``X(t)`` at time ``t``.
+        """
         # First, check if the time is in self.rvs.
         if t in self.rvs:
             return self.rvs[t]
         return super().__getitem__(t)
 
     def __call__(self, t):
+        """Return the random variable representing the process at time ``t``.
+
+        Parameters
+        ----------
+        t : int or float
+            The time index to evaluate.
+
+        Returns
+        -------
+        RV
+            The random variable ``X(t)`` at time ``t``.
+        """
         return RV(self.prob_space, lambda outcome: self.func(outcome)(t))
